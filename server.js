6d1ad835511bb858efaf46afbc2247f9d316eb41
Fabrizio Rossier <fabriziorossier@gmail.com>
2022-04-06 19:33:25 -0400

    Setup

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const jwt = require('jsonwebtoken');
+const secretKey = require('./secretKey');
+const {} = require('./SQLqueries');
+const port = process.env.PORT || 3000;
+const minutos = 1;
+let tokenUser = '';
+let tokenAdmin = '';
+
+// Setup - App
+app.listen(port, console.log(`Listening on port ${port}`));
+
+// Setup - Assets
+app.use('/assets', express.static(__dirname + '/assets'));
+
+// Setup - jQuery
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
+
+// Setup - Bootstrap
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/bootstrapJS', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+
+// Setup - express-handlebars
+app.engine('handlebars',
+    exphbs.engine({
+        layoutsDir: __dirname + '/views/layouts',
+        partialsDir: __dirname + '/views/components',
+        helpers: {
+            increment: function(value, options){ return parseInt(value) +1},
+        }
+    })
+);
+app.set('view engine', 'handlebars');
+
+// Setup - body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
