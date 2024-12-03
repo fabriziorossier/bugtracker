849d1cdfdb85ef25d2c63f63215f70b5750cec11
Fabrizio Rossier <fabriziorossier@gmail.com>
2024-12-03 20:23:25 -0300

    fix: deleted non-existing user ids

--- db_migrations/04_20220413.sql
+++ db_migrations/04_20220413.sql
@@ -4,5 +4,3 @@ ALTER TABLE usuario ADD estado BOOLEAN;
 UPDATE usuario SET estado = true WHERE ID_usuario = 1;
 UPDATE usuario SET estado = true WHERE ID_usuario = 2;
 UPDATE usuario SET estado = true WHERE ID_usuario = 3;
-UPDATE usuario SET estado = true WHERE ID_usuario = 4;
-UPDATE usuario SET estado = true WHERE ID_usuario = 5;
